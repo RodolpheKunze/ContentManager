7fbfdf70da7787e9c43cca5ae31cb7c226f8d29c
RodolpheKunze <kunze@free.fr>
2024-12-07 08:38:30 +0100

    elastic and minio docker compose

---
+++ secure-cms/cms-storage/src/main/java/com/example/storage/service/StorageProvider.java
@@ -0,0 +1,6 @@
+package com.example.storage.service;
+
+public interface StorageProvider {
+    void store(String key, byte[] content) throws Exception;
+    byte[] retrieve(String key) throws Exception;
+}
